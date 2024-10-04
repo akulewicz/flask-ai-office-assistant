50ceb3a249cb73209ba6067b1079d58666ba0d74
akulewicz <a.kulewicz@gmail.com>
2024-10-04 19:03:10 +0200

    Move secret key to .flaskenv, add .flaskenv.example, add requirements.txt file

--- aiassistant/__init__.py
+++ aiassistant/__init__.py
@@ -7,7 +7,7 @@ from flask_bcrypt import Bcrypt
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'sda32r4d3450ewfk-A4rejmcsd'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
